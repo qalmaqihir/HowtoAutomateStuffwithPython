349f62a86a983ce5ebfe72074ac6ac0c25916463
qalmaqihir <supine.me@gmail.com>
2022-09-10 14:18:43 +0600

    Scrapy tutorials

---
+++ tutorial/tutorial/spiders/quotes_spider_shorter.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class QuotesSpinder(scrapy.Spider):
+    name='quotes'
+    start_urls=[
+            'https://quotes.toscrape.com/page/1/',
+            'https://quotes.toscrape.com/page/2/',
+                ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").get(),
+                'author': quote.css("small.author::text").get(),
+                'tags':quote.css("div.tags a.tag::text").getall(),
+            }
+            next_page=response.css('li.next a::attr(href)').get()
+            if next_page is not None:
+                yield scrapy.follow(next_page, callback=self.parse)
+                ## can use the longer version below
+                # next_page=response.urljoin(next_page)
+                # yield scrapy.Request(next_page, callback=self.parse)
+
+
+
+        # page=response.url.split("/")[-2]
+        # filename=f'quotes-{page}.html'
+        # with open(filename,'wb') as f:
+        #     f.write(response.body)
+        # self.log(f'saved file {filename}')
